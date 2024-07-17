1f27abf9a699da63b1b1f345595c6936b561c80f
Stefan Woutersen <stefanwoutersen@gmail.com>
2024-07-17 09:29:15 +0000

    switched to postgres#

--- board_game_chronicle/settings.py
+++ board_game_chronicle/settings.py
@@ -97,17 +97,17 @@ WSGI_APPLICATION = 'board_game_chronicle.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     'default':
-#         dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    'default':
+        dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
